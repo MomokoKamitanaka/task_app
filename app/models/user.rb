fc4a828566cd437a9e699e47b1294708c0d379d5
Momokokamitanaka <54521397+kmtnk0131@users.noreply.github.com>
2019-12-13 15:33:04 +0900

    add_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 20 }
 end
+
+# presence: trueは値が空ではないということ確かめるバリデーション
+# また、length: { maximum: 20 }は値の文字数が最大20文字までということを表すバリデーション
