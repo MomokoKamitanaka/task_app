fec96f4807a0653db297dbd607e4c0ee11131241
Momokokamitanaka <54521397+kmtnk0131@users.noreply.github.com>
2019-12-21 01:42:52 +0900

    list_create #14

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,10 @@
 class TopController < ApplicationController
 
   def index
+    @lists = List.where(user: current_user).order("created_at ASC")
+
+    # List.where(user: current_user)は、user_idが現在ログインしているユーザー(current_user)のレコードを取得
+    # order("created_at ASC") 古い順の投稿一覧
   end
   
 end
