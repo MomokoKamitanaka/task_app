7fca523adbae2945ba1072d42edfa8f17c3f3706
Momokokamitanaka <54521397+kmtnk0131@users.noreply.github.com>
2019-12-23 00:03:55 +0900

    card_edit

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,4 +1,9 @@
 class CardController < ApplicationController
+
+before_action :set_card, only: [:show, :edit, :update]
+
+
+
   def new
     @card = Card.new
     @list = List.find_by(id: params[:list_id])
@@ -16,13 +21,28 @@ class CardController < ApplicationController
 
 
   def show
-    @card = Card.find_by(id: params[:id])
   end
+
+
+  def edit
+  end
+
+  def update
+    if @card.update_attributes(card_params)
+      redirect_to :root
+    else
+      render action: :edit
+  end
+end
 
 
   private
     def card_params
       params.require(:card).permit(:title, :memo, :list_id)
+    end
+
+    def set_card
+      @card = Card.find_by(id: params[:id])
     end
   
 end
